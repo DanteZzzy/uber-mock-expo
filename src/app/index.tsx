a2d9e374ded573c7ec535f93e56de0f2c0d2882f
DanteZzzy <gabrieltf04@gmail.com>
2026-09-20 20:32:22 -0300

    Tela de Login: estrutura dos componentes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,57 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import { Pressable, Text, TextInput, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function Index() {
+export default function Login() {
+  const [contato, setContato] = useState("");
+
+  function continuar() {
+    // Quando a Home existir, vamos navegar para ela aqui
+    console.log("Continuar com:", contato);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View>
+      <View>
+        <Text>Uber</Text>
+      </View>
+
+      <View>
+        <Text>Qual é o seu número de telefone ou e-mail?</Text>
+
+        <TextInput
+          placeholder="Informar telefone ou e-mail"
+          value={contato}
+          onChangeText={setContato}
+          autoCapitalize="none"
+        />
+
+        <Pressable onPress={continuar}>
+          <Text>Continuar</Text>
+        </Pressable>
+
+        <View>
+          <View />
+          <Text>ou</Text>
+          <View />
+        </View>
+
+        <Pressable>
+          <Ionicons name="logo-google" size={20} />
+          <Text>Continuar com o Google</Text>
+        </Pressable>
+
+        <Pressable>
+          <Ionicons name="logo-apple" size={20} />
+          <Text>Continuar com a Apple</Text>
+        </Pressable>
+
+        <Text>
+          Você concorda em receber um código de verificação por mensagem de
+          texto ou WhatsApp. Sujeito a cobranças de tarifas por uso de dados e
+          envio de mensagens.
+        </Text>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
